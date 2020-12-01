9fc82627cf17e750e4faa078e6fbc3da75149f9c
KaranP99 <51881466+KaranP99@users.noreply.github.com>
2020-12-01 11:42:58 +0000

    java - Mbed
    Java code which links to a Mbed board, turning the Mbed board into a piano, each button is a different note on the piano

---
+++ Mbed Sounds/code/Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    private Main(){}
+    
+    public static void main(String[] args)
+    {
+        MusicOrganizer prog = new MusicOrganizer();
+        prog.run();
+        prog.finish();
+        System.exit(0);
+    }
+}
